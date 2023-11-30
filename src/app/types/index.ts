81c7873bc1688957d56bf9765c6895c15e79414d
Valmir Junior <jnrplay@hotmail.com>
2023-11-30 05:17:45 -0300

    refactor: only used data

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -4,13 +4,6 @@ export type Model = {
 };
 
 export type ModelCurrentValue = {
-  TipoVeiculo: number;
   Valor: string;
-  Marca: string;
   Modelo: string;
-  AnoModelo: number;
-  Combustivel: string;
-  CodigoFipe: string;
-  MesReferencia: string;
-  SiglaCombustivel: string;
 };
